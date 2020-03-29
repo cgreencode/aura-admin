3b79b29669b1d64f12885a914c86f5839225414f
anproagile <anproagile@gmail.com>
2020-03-29 22:17:49 +0530

    firebase hosting cache

--- src/views/Config.vue
+++ src/views/Config.vue
@@ -30,7 +30,6 @@
       <v-tab-item key="item3">
         <v-card flat>
           <v-card-text>
-            
             <p>Item 3</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores illo blanditiis iste atque accusamus expedita? Veniam, cum! Quia magnam quisquam aliquid, illo totam incidunt! Ea, fuga error? Numquam, nostrum ut.</p>
           </v-card-text>
@@ -39,7 +38,8 @@
       <v-tab-item key="item4">
         <v-card flat>
           <v-card-text>
-            <Partners @show="showSnakeBar"/>
+            <p>Item 4</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores illo blanditiis iste atque accusamus expedita? Veniam, cum! Quia magnam quisquam aliquid, illo totam incidunt! Ea, fuga error? Numquam, nostrum ut.</p>
           </v-card-text>
         </v-card>
       </v-tab-item>
@@ -60,15 +60,13 @@ import Snakebar from "@/components/Common/Snakebar";
 
 import General from "@/components/Config/General/General";
 import CommunityGuidelines from "@/components/Config/CommunityGuidelines";
-import Partners from "@/components/Config/Partners/Partners";
 
 export default {
   name: "Config",
   components: {
     General,
     CommunityGuidelines,
-    Snakebar,
-    Partners
+    Snakebar
   },
   data: () => ({
     tab: null,
